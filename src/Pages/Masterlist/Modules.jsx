c41a7fcce6995da5716f4e8ba2baae89d73ea23e
Humer <humz024@gmail.com>
2023-02-02 14:54:25 +0800

    masterlist user accounts

---
+++ src/Pages/Masterlist/Modules.jsx
@@ -0,0 +1,263 @@
+import React, { useState } from "react";
+import "../../Style/Masterlist/modules.scss";
+import Moment from "moment";
+import MasterlistToolbar from "../../Components/Reusable/MasterlistToolbar";
+import ActionMenu from "../../Components/Reusable/ActionMenu";
+import AddModules from "./AddEdit/AddModules";
+
+// RTK
+import { useDispatch } from "react-redux";
+import { openToast } from "../../Redux/StateManagement/toastSlice";
+import {
+  openConfirm,
+  closeConfirm,
+} from "../../Redux/StateManagement/confirmSlice";
+import { usePostModuleStatusApiMutation } from "../../Redux/Query/ModulesApi";
+import { useGetModulesApiQuery } from "../../Redux/Query/ModulesApi";
+
+import { useSelector } from "react-redux";
+
+// MUI
+import {
+  Box,
+  Dialog,
+  SwipeableDrawer,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Help, ReportProblem } from "@mui/icons-material";
+
+const Modules = () => {
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("active");
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
+  const [updateModule, setUpdateModule] = useState({
+    status: false,
+    id: null,
+    module_name: "",
+  });
+
+  const drawer = useSelector((state) => state.drawer);
+
+  const limitHandler = (e) => {
+    setLimit(parseInt(e.target.value));
+  };
+
+  const pageHandler = (_, page) => {
+    console.log(page + 1);
+    setPage(page + 1);
+  };
+
+  const {
+    data: modules,
+    isLoading: modulesLoading,
+    isSuccess: modulesSuccess,
+    isError: modulesError,
+  } = useGetModulesApiQuery(
+    {
+      page: page,
+      limit: limit,
+      status: status,
+      search: search,
+    },
+    { refetchOnMountOrArgChange: true }
+  );
+
+  const [postModuleStatusApi, { isLoading }] = usePostModuleStatusApiMutation();
+
+  const dispatch = useDispatch();
+
+  const onArchiveRestoreHandler = async (id) => {
+    dispatch(
+      openConfirm({
+        icon: status === "active" ? ReportProblem : Help,
+        message: (
+          <Box>
+            <Typography> Are you sure you want to</Typography>
+            <Typography
+              sx={{
+                display: "inline-block",
+                color: "secondary.main",
+                fontWeight: "bold",
+              }}
+            >
+              {status === "active" ? "ARCHIVE" : "ACTIVATE"}
+            </Typography>{" "}
+            this data?
+          </Box>
+        ),
+
+        onConfirm: async () => {
+          try {
+            const result = await postModuleStatusApi({
+              id: id,
+              status: status === "active" ? false : true,
+            }).unwrap();
+
+            dispatch(
+              openToast({
+                message: result.message,
+                duration: 5000,
+              })
+            );
+            dispatch(closeConfirm());
+          } catch (err) {
+            console.log(err.message);
+          }
+        },
+      })
+    );
+  };
+
+  const onUpdateHandler = (props) => {
+    const { id, module_name } = props;
+    setUpdateModule({
+      status: true,
+      id: id,
+      module_name: module_name,
+    });
+  };
+
+  const onSetPage = () => {
+    setPage(1);
+  };
+
+  return (
+    <Box className="mcontainer">
+      <Typography sx={{ fontFamily: "Anton", fontSize: "2rem" }}>
+        Modules
+      </Typography>
+
+      <Box className="mcontainer__wrapper">
+        <MasterlistToolbar
+          path="#"
+          onStatusChange={setStatus}
+          onSearchChange={setSearch}
+          onSetPage={setPage}
+        />
+
+        <Box>
+          <TableContainer>
+            <Table className="mcontainer__table" stickyHeader>
+              <TableHead>
+                <TableRow>
+                  <TableCell className="mcontainer__th-cell mcontainer__text-center">
+                    Id
+                  </TableCell>
+
+                  <TableCell className="mcontainer__th-cell">Module</TableCell>
+
+                  <TableCell className="mcontainer__th-cell mcontainer__text-center">
+                    Status
+                  </TableCell>
+
+                  <TableCell
+                    className="mcontainer__th-cell mcontainer__text-center"
+                    sx={{ whiteSpace: "nowrap" }}
+                  >
+                    Date Created
+                  </TableCell>
+
+                  <TableCell className="mcontainer__th-cell mcontainer__text-center">
+                    Action
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {modulesSuccess &&
+                  modules.data.map((modules) => (
+                    <TableRow key={modules.id}>
+                      <TableCell className="mcontainer__tr-cell mcontainer__text-center">
+                        {modules.id}
+                      </TableCell>
+
+                      <TableCell className="mcontainer__tr-cell mcontainer__text-weight">
+                        {modules.module_name}
+                      </TableCell>
+
+                      <TableCell className="mcontainer__tr-cell mcontainer__text-center">
+                        {modules.is_active ? (
+                          <Typography
+                            color="success.main"
+                            sx={{
+                              px: 1,
+                              maxWidth: "10ch",
+                              margin: "0 auto",
+                              fontSize: "13px",
+                              background: "#26f57c2a",
+                              borderRadius: "8px",
+                            }}
+                          >
+                            ACTIVE
+                          </Typography>
+                        ) : (
+                          <Typography
+                            align="center"
+                            color="errorColor.main"
+                            sx={{
+                              px: 1,
+                              maxWidth: "10ch",
+                              margin: "0 auto",
+                              fontSize: "13px",
+                              background: "#fc3e3e34",
+                              borderRadius: "8px",
+                            }}
+                          >
+                            INACTIVE
+                          </Typography>
+                        )}
+                      </TableCell>
+
+                      <TableCell className="mcontainer__tr-cell mcontainer__text-center">
+                        {Moment(modules.created_at).format("MMM DD, YYYY")}
+                      </TableCell>
+
+                      <TableCell className="mcontainer__tr-cell mcontainer__text-center ">
+                        <ActionMenu
+                          status={status}
+                          data={modules}
+                          onUpdateHandler={onUpdateHandler}
+                          onArchiveRestoreHandler={onArchiveRestoreHandler}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+
+        <Box className="mcontainer__pagination">
+          <TablePagination
+            rowsPerPageOptions={[
+              5,
+              10,
+              15,
+              { label: "All", value: parseInt(modules?.total) },
+            ]}
+            component="div"
+            count={modulesSuccess ? modules.total : 0}
+            page={modulesSuccess ? modules.current_page - 1 : 0}
+            rowsPerPage={modulesSuccess ? parseInt(modules?.per_page) : 5}
+            onPageChange={pageHandler}
+            onRowsPerPageChange={limitHandler}
+          />
+        </Box>
+      </Box>
+
+      <Dialog open={drawer} PaperProps={{ sx: { borderRadius: "10px" } }}>
+        <AddModules data={updateModule} />
+      </Dialog>
+    </Box>
+  );
+};
+
+export default Modules;
